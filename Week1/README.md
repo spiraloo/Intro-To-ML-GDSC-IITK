663eb7ece20b49a134eab74604b7cd9540c3472f
Harshita <harshitachaudhary0607@gmail.com>
2023-12-14 00:28:10 +0530

    adding HEllo world
    This is to learn to use git. Ignore for evaluation.

--- Week1/README.md
+++ Week1/README.md
@@ -1,1 +1,5 @@
 # This is the folder to submit Week 1 exercises
+
+##subheader
+
+watch bluelock on netflix.
---
+++ Week1/hello.py
@@ -0,0 +1,4 @@
+#This is ME trying to learn code and put it in git!
+#This is not a code to any exercise in WEEK 1 !!
+#PLEASE IGNORE !!! 
+print("Helloo world")
